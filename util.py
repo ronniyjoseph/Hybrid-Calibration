17a26c272325f985e72f20539d39c98deb5f6bd7
ronniyjoseph <ronniyjoseph@gmail.com>
2019-08-16 09:02:59 +0800

    Added new function that generates covariance vectors for you

--- util.py
+++ util.py
@@ -1,5 +1,10 @@
+import sys
 import numpy
+
+sys.path.append("../../beam_perturbations/code/tile_beam_perturbations/")
+
 from skymodel import apparent_fluxes_numba
+from analytic_covariance import sky_covariance
 
 def split_visibility(data):
     data_real = numpy.real(data)
@@ -18,12 +23,21 @@ def find_sky_model_sources(sky_realisation, frequency_range, antenna_size = 4):
     return sky_model
 
 
-def generate_sky_model_vectors(sky_model_sources, baseline_table, frequency_range, antenna_size):
+def generate_sky_model_vectors(sky_model_sources, baseline_table, frequency_range, antenna_size, sorting_indices = None):
     number_of_sources = len(sky_model_sources.fluxes)
     sky_vectors = numpy.zeros((number_of_sources, baseline_table.number_of_baselines*2))
     for i in range(number_of_sources):
         single_source = sky_model_sources.select_sources(i)
         source_visibilities = single_source.create_visibility_model(baseline_table, frequency_range, antenna_size)
-        sky_vectors[i, :] = split_visibility(source_visibilities)
+        if sorting_indices is not None:
+            sky_vectors[i, :] = split_visibility(source_visibilities[sorting_indices])
 
     return sky_vectors
+
+def generate_covariance_vectors(number_of_baselines, frequency_range):
+    covariance_vectors = numpy.zeros((2, number_of_baselines*2))
+    covariance_vectors[0::2] = 1
+    covariance_vectors[1::2] = 1
+    covariance_vectors *= sky_covariance(0, 0, frequency_range)
+
+    return covariance_vectors
