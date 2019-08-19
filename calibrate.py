cfb97fcd3c04f374d5e9fb61ceb25935408f07f9
ronniyjoseph <ronniyjoseph@gmail.com>
2019-08-19 10:48:52 +0800

    Added verbosity option to silence output of scipy fmin_cg

--- calibrate.py
+++ calibrate.py
@@ -9,7 +9,7 @@ from corrcal2 import get_gradient
 
 
 def hybrid_calibration(data, noise_variance,covariance_vectors, model_vectors, edges, antenna1_indices, antenna2_indices,
-              gain_guess = None, scale_factor = 1000):
+              gain_guess = None, scale_factor = 1000, verbose = False):
     if gain_guess is None:
         #compute number of antennas based on number of baselines
         n_antennas = int((1 + numpy.sqrt(1 + 4*len(noise_variance)))/2)
@@ -19,7 +19,8 @@ def hybrid_calibration(data, noise_variance,covariance_vectors, model_vectors, e
     sparse_matrix_object = sparse_2level(noise_variance, covariance_vectors, model_vectors, edges)
 
     gain_solutions_split = 1/scale_factor*fmin_cg(get_chisq, gain_guess * scale_factor, get_gradient,
-                                   (data, sparse_matrix_object, antenna1_indices, antenna2_indices, scale_factor))
+                                   (data, sparse_matrix_object, antenna1_indices, antenna2_indices, scale_factor),
+                                                  disp = verbose)
     gain_solutions_complex = gain_solutions_split[::2] + 1j*gain_solutions_split[1::2]
 
     return gain_solutions_complex
