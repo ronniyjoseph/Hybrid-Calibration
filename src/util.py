3497523bbbf7821a7d4e60b9952ea392c49bbb09
ronniyjoseph <ronniyjoseph@gmail.com>
2019-10-24 14:23:52 +0800

    Added object compatible version of redundant baseline finder

--- src/util.py
+++ src/util.py
@@ -1,9 +1,10 @@
 import sys
 import numpy
+from .radiotelescope import BaselineTable
+from .skymodel import apparent_fluxes_numba
 
 sys.path.append("../../beam_perturbations/code/tile_beam_perturbations/")
 
-from skymodel import apparent_fluxes_numba
 from analytic_covariance import sky_covariance
 
 def split_visibility(data):
@@ -58,3 +59,86 @@ def hexagonal_array(nside):
 
     return numpy.array(antpos)
 
+
+def redundant_baseline_finder(baseline_table_object, group_minimum=3, threshold=1 / 6, verbose=False):
+    """
+
+    antenna_id1:
+    antenna_id2:
+    u:
+    v:
+    w:
+    baseline_direction:
+    group_minimum:
+    threshold:
+    verbose:
+    :return:
+    """
+    antenna_id1 = baseline_table_object.antenna_id1
+    antenna_id2 = baseline_table_object.antenna_id2
+    u = baseline_table_object.u_coordinates
+    v = baseline_table_object.v_coordinates
+    w = baseline_table_object.w_coordinates
+
+    n_baselines = u.shape[0]
+    # create empty table
+    redundant_baselines = numpy.zeros((n_baselines, 6))
+    # arbitrary counters
+    # Let's find all the redundant baselines within our threshold
+    group_counter = 0
+    k = 0
+    # Go through all antennas, take each antenna out and all antennas
+    # which are part of the not redundant enough group
+    while u.shape[0] > 0:
+        # calculate uv separation at the calibration wavelength
+        separation = numpy.sqrt((u - u[0]) ** 2. + (v - v[0]) ** 2.)
+        # find all baselines within the lambda fraction
+        select_indices = numpy.where(separation <= threshold)
+
+        # is this number larger than the minimum number
+        if len(select_indices[0]) >= group_minimum:
+            # go through the selected baselines
+
+            for i in range(len(select_indices[0])):
+                # add antenna number
+                redundant_baselines[k, 0] = antenna_id1[select_indices[0][i]]
+                redundant_baselines[k, 1] = antenna_id2[select_indices[0][i]]
+                # add coordinates uvw
+                redundant_baselines[k, 2] = u[select_indices[0][i]]
+                redundant_baselines[k, 3] = v[select_indices[0][i]]
+                redundant_baselines[k, 4] = w[select_indices[0][i]]
+                # add baseline group identifier
+                redundant_baselines[k, 5] = 50000000 + 52 * (group_counter + 1)
+                k += 1
+            group_counter += 1
+        # update the list, take out the used antennas
+        all_indices = numpy.arange(len(u))
+        unselected_indices = numpy.setdiff1d(all_indices, select_indices[0])
+
+        antenna_id1 = antenna_id1[unselected_indices]
+        antenna_id2 = antenna_id2[unselected_indices]
+        u = u[unselected_indices]
+        v = v[unselected_indices]
+        w = w[unselected_indices]
+
+    if verbose:
+        print("There are", k, "redundant baselines in this array.")
+        print("There are", group_counter, "redundant groups in this array")
+
+    # find the filled entries
+    non_zero_indices = numpy.where(redundant_baselines[:, 0] != 0)
+    # remove the empty entries
+    redundant_baselines = redundant_baselines[non_zero_indices[0], :]
+
+    table_object = BaselineTable()
+    table_object.antenna_id1 = redundant_baselines[:, 0]
+    table_object.antenna_id2 = redundant_baselines[:, 1]
+    table_object.u_coordinates = redundant_baselines[:, 2]
+    table_object.v_coordinates = redundant_baselines[:, 3]
+    table_object.w_coordinates = redundant_baselines[:, 4]
+    table_object.group_indices = redundant_baselines[:, 5]
+
+    table_object.reference_frequency = 150e6
+    table_object.number_of_baselines = len(redundant_baselines[:, 0])
+
+    return table_object
