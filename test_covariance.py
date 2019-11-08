c65d84a969f7f7716cd2cdfad49e30bc45979b96
ronniyjoseph <ronniyjoseph@gmail.com>
2019-11-08 15:54:17 +0800

    Added matrix stability test

--- test_covariance.py
+++ test_covariance.py
@@ -1,4 +1,6 @@
 import numpy
+from scipy.constants import c
+
 import sys
 from src.radiotelescope import RadioTelescope
 from src.radiotelescope import AntennaPositions
@@ -11,6 +13,10 @@ from src.covariance import sky_covariance
 from src.covariance import blackman_harris_taper
 from src.covariance import dft_matrix
 from src.covariance import compute_ps_variance
+from src.covariance import thermal_variance
+
+from cramer_rao_bound import redundant_matrix_populator
+
 from matplotlib import pyplot
 from matplotlib import colors
 
@@ -18,6 +24,7 @@ from src.plottools import plot_power_spectrum
 
 from src.radiotelescope import beam_width
 from src.skymodel import sky_moment_returner
+
 
 def calculate_beam_power_spectrum(u, nu, save=False, plot_name="beam_2D_ps.pdf"):
     diameter = 4
@@ -88,7 +95,7 @@ def sky_covariance_old(u, v, nu, S_low=0.1, S_mid=1, S_high=1):
     sigma_nu = width_tile**2/2
     print(f"Old Beam width {sigma_nu}")
 
-    mu_2_r = sky_moment_returner(2, S_low=S_low, S_mid=S_mid, S_high=S_high)
+    mu_2_r = sky_moment_returner(2, s_low=S_low, s_mid=S_mid, s_high=S_high)
 
     sky_covariance = 2 * numpy.pi * mu_2_r * sigma_nu * numpy.exp(
         -2*numpy.pi ** 2 * sigma_nu * ((uu1 - uu2) ** 2 + (vv1 - vv2) ** 2))
@@ -126,20 +133,74 @@ def test_baseline_covariance(nu = 150e6):
     return
 
 
+def test_matrix_stability(nu=150e6):
+    position_precision =  1e-1
+    sky_model_depth = 1e0
+    uv_scales = numpy.array([0, 0])
+    non_redundant_block = sky_covariance(nu=nu, u=uv_scales, v=uv_scales, S_high=sky_model_depth,
+                                         mode='baseline')
+    print("Non Redundant Block")
+    print(f"Condition Number: {numpy.linalg.cond(non_redundant_block)}")
+    print(non_redundant_block)
+    print(numpy.linalg.pinv(non_redundant_block))
+
+
+    non_redundant_block += numpy.diag(numpy.zeros(len(uv_scales)) + thermal_variance())
+    print("")
+    print("Non Redundant Block + Thermal Noise")
+    print(f"Condition Number: {numpy.linalg.cond(non_redundant_block)}")
+    print(non_redundant_block)
+    print(numpy.linalg.pinv(non_redundant_block))
+
+
+    uv_scales = numpy.array([0, position_precision/c*nu])
+    non_redundant_block = sky_covariance(nu=nu, u=uv_scales, v=uv_scales, S_high=sky_model_depth,
+                                         mode='baseline')
+    print("Non Redundant Block")
+    print(f"Condition Number: {numpy.linalg.cond(non_redundant_block)}")
+    print(non_redundant_block)
+    print(numpy.linalg.pinv(non_redundant_block))
+
+    non_redundant_block += numpy.diag(numpy.zeros(len(uv_scales)) + thermal_variance())
+    print("")
+    print("Non Redundant Block + Thermal Noise")
+    print(f"Condition Number: {numpy.linalg.cond(non_redundant_block)}")
+    print(non_redundant_block)
+    print(numpy.linalg.pinv(non_redundant_block))
+
+    return
+
+
+def test_jacobian_stability():
+
+    antenna_positions = hexagonal_array(5)
+    antenna_table = AntennaPositions(load=False)
+    antenna_table.antenna_ids = numpy.arange(0, antenna_positions.shape[0], 1)
+    antenna_table.x_coordinates = antenna_positions[:, 0]
+    antenna_table.y_coordinates = antenna_positions[:, 1]
+    antenna_table.z_coordinates = antenna_positions[:, 2]
+    baseline_table = BaselineTable(position_table=antenna_table)
+
+    redundant_baselines = redundant_baseline_finder(baseline_table)
+    # skymodel_baselines = redundant_baseline_finder(baseline_table, group_minimum=1)
+
+    jacobian_gain_matrix, red_tiles, red_groups = redundant_matrix_populator(redundant_baselines)
+    jacobian_copy = jacobian_gain_matrix.copy()
+
+    jacobian_gain_matrix[:, :len(red_tiles)] *= numpy.sqrt(sky_moment_returner(n_order=2, s_low=1))
+    jacobian_copy[:, :len(red_tiles)] *= numpy.sqrt(sky_moment_returner(n_order=2, s_low=0.9))
+
+    print(numpy.linalg.cond(jacobian_gain_matrix))
+    print(numpy.linalg.cond(jacobian_copy))
+
+    print(numpy.sum(numpy.linalg.pinv(jacobian_gain_matrix) - numpy.linalg.pinv(jacobian_copy)))
+    pyplot.imshow(numpy.linalg.pinv(jacobian_gain_matrix) - numpy.linalg.pinv(jacobian_copy))
+    pyplot.show()
+    return
+
+
 if __name__ == "__main__":
     # compare_power_spectrum()
-    test_baseline_covariance()
-
-
-
-
-
-    # nu = 150e6
-    # u = redundant_table.u(nu)
-    # v = redundant_table.v(nu)
-    # mode = 'baseline'
-    # calculate_beam_power_spectrum(u, nu)
-    #
-    #
-    # pyplot.imshow(cov)
-    # pyplot.show()
+    # test_baseline_covariance()
+    test_matrix_stability()
+    # test_jacobian_stability()
