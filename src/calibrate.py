d65585e03deb7b0c27b93d69741ca1fded07a9ab
ronniyjoseph <ronniyjoseph@gmail.com>
2020-01-29 13:10:30 +0800

    Updates import of corrcal function to latest version

--- src/calibrate.py
+++ src/calibrate.py
@@ -2,10 +2,10 @@ import sys
 import numpy
 from scipy.optimize import fmin_cg
 
-sys.path.append("../corrcal2")
-from corrcal2 import sparse_2level
-from corrcal2 import get_chisq
-from corrcal2 import get_gradient
+sys.path.append("../../CorrCal_UKZN_Development/corrcal")
+from corrcal import Sparse2Level
+from corrcal import get_chisq
+from corrcal import get_gradient
 
 
 def hybrid_calibration(data, noise_variance,covariance_vectors, model_vectors, edges, antenna1_indices, antenna2_indices,
@@ -16,10 +16,10 @@ def hybrid_calibration(data, noise_variance,covariance_vectors, model_vectors, e
         gain_guess = numpy.zeros(2 * n_antennas)
         gain_guess[::2] = 1
 
-    sparse_matrix_object = sparse_2level(noise_variance, covariance_vectors, model_vectors, edges)
+    sparse_matrix_object = Sparse2Level(noise_variance, covariance_vectors, model_vectors, edges)
 
     get_chisq(gain_guess * scale_factor, data, sparse_matrix_object, antenna1_indices, antenna2_indices, scale_fac=scale_factor)
-    get_gradient(ggain_guess * scale_factor, data, sparse_matrix_object, antenna1_indices, antenna2_indices, scale_fac=scale_factor)
+    get_gradient(gain_guess * scale_factor, data, sparse_matrix_object, antenna1_indices, antenna2_indices, scale_factor=scale_factor)
 
     gain_solutions_split = 1/scale_factor*fmin_cg(get_chisq, gain_guess * scale_factor, get_gradient,
                                    (data, sparse_matrix_object, antenna1_indices, antenna2_indices, scale_factor),
