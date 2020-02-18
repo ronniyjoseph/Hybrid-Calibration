0bee4b4d95604527679a2a2aa6c7c5b257081d32
ronniyjoseph <ronniyjoseph@gmail.com>
2020-02-19 07:45:58 +0800

    Adapted to read in telescope positions from simulated data

--- Plot_Recompute_Input_Visibilities.py
+++ Plot_Recompute_Input_Visibilities.py
@@ -10,20 +10,24 @@ from corrcal import grid_data
 
 def main():
     data_type = "noise_visibilities"
-    path = "/data/rjoseph/Hybrid_Calibration/numerical_simulations/Initial_Testing2_Gain_Unity/"
+    path = "/data/rjoseph/Hybrid_Calibration/numerical_simulations/Linear_No_Models/"
     input_parameters = numpy.loadtxt(path + "input_parameters.txt")
     n_realisations = 100 #input_parameters[-1]
     ideal_visibilities = load_data(path, 'ideal_visibilities', n_realisations)
     noise_visibilities = load_data(path, 'noise_visibilities', n_realisations)
-    measured_visibilities = load_data(path, "measured_visibilties", n_realisations)
-    antenna_index = 2
+    measured_visibilities = load_data(path, "measured_visibilities", n_realisations)
 
+    telescope_positions = numpy.load(path+"realisation_0/telescope_positions.npy")
+    telescope_gains = numpy.load(path+"realisation_0/telescope_gains.npy")
+    antenna_table = AntennaPositions(load=False)
 
+    antenna_table.antenna_ids = telescope_positions[0, :]
+    antenna_table.x_coordinates = telescope_positions[1, :]
+    antenna_table.y_coordinates = telescope_positions[2, :]
+    antenna_table.z_coordinates = telescope_positions[3, :]
+    antenna_table.antenna_gains = telescope_gains
 
-    print(ideal_visibilities.shape)
-    antenna_table = AntennaPositions(load=False, shape=['doublehex', 14, 0, 0, 100, 250])
     baseline_table = BaselineTable(position_table=antenna_table, frequency_channels=numpy.array([150e6]))
-    print(len(baseline_table.u_coordinates))
     data_sorted, u_sorted, v_sorted, noise_sorted, ant1_sorted, ant2_sorted, edges_sorted, sorting_indices, \
     conjugation_flag = grid_data(ideal_visibilities[:, 0],
                                  baseline_table.u_coordinates,
@@ -32,19 +36,19 @@ def main():
                                  baseline_table.antenna_id1.astype(int),
                                  baseline_table.antenna_id2.astype(int))
 
-    sky_covariance = numpy.cov(ideal_visibilities[sorting_indices, :], ideal_visibilities[sorting_indices, :])
-    noise_covariance = numpy.cov(noise_visibilities[sorting_indices, :], noise_visibilities[sorting_indices, :])
-    data_covariance = numpy.cov(measured_visibilities[sorting_indices, :], measured_visibilities[sorting_indices, :])
+    sky_covariance = numpy.cov(ideal_visibilities[sorting_indices, :])
+    noise_covariance = numpy.cov(noise_visibilities[sorting_indices, :])
+    data_covariance = numpy.cov(measured_visibilities[sorting_indices, :])
 
     figure, axes = pyplot.subplots(2, 3, figsize=(15, 10))
 
-    axes[0, 0].imshow(numpy.real(sky_covariance))
-    axes[0, 1].imshow(numpy.real(noise_covariance))
-    axes[0, 2].imshow(numpy.real(data_covariance))
+    axes[0, 0].imshow(numpy.real(sky_covariance), interpolation=None)
+    axes[0, 1].imshow(numpy.real(noise_covariance), interpolation=None)
+    axes[0, 2].imshow(numpy.real(data_covariance), interpolation=None)
 
-    axes[1, 0].imshow(numpy.imag(sky_covariance))
-    axes[1, 1].imshow(numpy.imag(noise_covariance))
-    axes[1, 2].imshow(numpy.imag(data_covariance))
+    axes[1, 0].imshow(numpy.imag(sky_covariance), interpolation=None)
+    axes[1, 1].imshow(numpy.imag(noise_covariance), interpolation=None)
+    axes[1, 2].imshow(numpy.imag(data_covariance), interpolation=None)
 
     # axes[0, 0].hist(amplitudes, bins=100)
     # axes[0, 1].hist(phases, bins=100)
