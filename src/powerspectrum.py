d8492934e0ae6f2dcf5c8d11512ff0a8a0f0966f
ronniyjoseph <ronniyjoseph@gmail.com>
2020-03-12 13:23:01 +0800

    Moved taper and dft into compute power

--- src/powerspectrum.py
+++ src/powerspectrum.py
@@ -2,12 +2,16 @@ import numpy
 from scipy import signal
 
 
-def dft_matrix(nu):
+def discrete_fourier_transform_matrix(nu):
     dft = numpy.exp(-2 * numpy.pi * 1j / len(nu)) ** numpy.arange(0, len(nu), 1)
     dftmatrix = numpy.vander(dft, increasing=True) / numpy.sqrt(len(nu))
 
+    return dftmatrix
+
+
+def from_frequency_to_eta(nu):
     eta = numpy.arange(0, len(nu), 1) / (nu.max() - nu.min())
-    return dftmatrix, eta
+    return eta
 
 
 def blackman_harris_taper(frequency_range):
@@ -15,9 +19,14 @@ def blackman_harris_taper(frequency_range):
     return window
 
 
-def compute_ps_variance(taper1, taper2, covariance, dft_matrix):
+def compute_power(nu, covariance):
+    dft_matrix = discrete_fourier_transform_matrix(nu)
+    window = blackman_harris_taper(nu)
+    taper1, taper2 = numpy.meshgrid(window, window)
+
     tapered_cov = covariance * taper1 * taper2
     eta_cov = numpy.dot(numpy.dot(dft_matrix.conj().T, tapered_cov), dft_matrix)
-    variance = numpy.diag(numpy.real(eta_cov))
+    power = numpy.diag(numpy.real(eta_cov))[:int(len(nu) / 2)]
 
-    return variance
+    return power
+
