4a55cb38dd43aa41b67ba4dbd0ceec4853584966
ronniyjoseph <ronniyjoseph@gmail.com>
2020-03-17 09:36:22 +0800

    Added fidicual power spectrum functions

---
+++ Plot_Calibrated_Residuals_Redundant.py
@@ -0,0 +1,134 @@
+import numpy
+import argparse
+from matplotlib import colors
+
+from src.covariance import sky_covariance
+from src.covariance import position_covariance
+from src.covariance import beam_covariance
+from src.powerspectrum import compute_power
+from src.covariance import gain_error_covariance
+from src.powerspectrum import from_frequency_to_eta
+from src.plottools import plot_power_spectrum
+from src.plottools import plot_power_contours
+from src.powerspectrum import fiducial_eor_power_spectrum
+from src.generaltools import from_jansky_to_milikelvin
+
+def main(labelfontsize = 16, ticksize= 11):
+    output_path = "/data/rjoseph/Hybrid_Calibration/theoretical_calculations/power_spectra/"
+    k_perp_range = numpy.array([1e-4, 1.1e-1])
+    u_range = numpy.logspace(-1, numpy.log10(500), 100)
+    frequency_range = numpy.linspace(135, 165, 251) * 1e6
+    contour_levels = numpy.array([5e0, 1e1, 1e2, 1e3, 1e4])
+
+    eta = from_frequency_to_eta(frequency_range)
+
+    eor_power_spectrum = fiducial_eor_power_spectrum(u_range, eta)
+
+    position_raw, position_calibrated = calculate_residual_error(u=u_range, nu=frequency_range, residuals='position', calibration_type='redundant')
+    beam_raw, beam_calibrated = calculate_residual_error(u=u_range, nu=frequency_range, residuals='beam',calibration_type='redundant')
+    total_raw, total_calibrated = calculate_residual_error(u=u_range, nu=frequency_range, residuals='both',calibration_type='redundant')
+
+    figure, axes = pyplot.subplots(1, 3, figsize=(15, 5))
+
+    ps_norm = colors.LogNorm(vmin=1e3, vmax=1e15)
+
+    plot_power_spectrum(u_range, eta, frequency_range, position_calibrated, title="Position Error", axes=axes[0],
+                        axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
+                        xlabel_show=True, norm=ps_norm, ylabel_show=True)
+
+    plot_power_spectrum(u_range, eta, frequency_range, beam_calibrated, title="Beam Variations", axes=axes[1],
+                        axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
+                        xlabel_show=True, norm=ps_norm)
+
+    plot_power_spectrum(u_range, eta, frequency_range, total_calibrated, title="Total Error", axes=axes[2],
+                        axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
+                        xlabel_show=True, norm=ps_norm, zlabel_show=True)
+
+    plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(position_calibrated, frequency_range)/eor_power_spectrum,
+                        axes=axes[0], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True, norm=ps_norm, ylabel_show=True, contour_levels=contour_levels)
+
+    plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(beam_calibrated, frequency_range)/eor_power_spectrum,
+                        axes=axes[1], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True, norm=ps_norm, ylabel_show=True, contour_levels=contour_levels)
+
+    plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(total_calibrated, frequency_range)/eor_power_spectrum,
+                        axes=axes[2], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True, norm=ps_norm, ylabel_show=True, contour_levels=contour_levels)
+
+    pyplot.savefig(output_path + "Calibrated_Residuals_Redundant.pdf")
+    pyplot.show()
+    return
+
+
+def calculate_residual_error(u, nu, residuals='both', broken_baselines_weight = 1, weights = None,
+                             calibration_type = 'sky'):
+    cal_variance = numpy.zeros((len(u), int(len(nu) / 2)))
+    raw_variance = numpy.zeros((len(u), int(len(nu) / 2)))
+
+    gain_averaged_covariance = gain_error_covariance(u, nu, residuals=residuals, weights= weights,
+                                                     broken_baseline_weight = broken_baselines_weight,
+                                                     calibration_type=calibration_type)
+
+    # Compute the gain corrected residuals at all u scales
+    # if residuals == "position":
+    #     residual_variance = position_covariance(0, 0, frequency_range)
+    # elif residuals == "beam":
+    #     residual_variance = broken_baselines_weight **2*beam_covariance(0, v=0, nu=frequency_range)
+    # elif residuals == 'both':
+    #     residual_variance = sky_covariance(0, 0, frequency_range) + \
+    #                         broken_baselines_weight **2*beam_covariance(0, v=0, nu=frequency_range)
+    #
+    # gain = residual_variance / sky_covariance(0, 0, frequency_range)
+
+    for i in range(len(u)):
+        if calibration_type == "redundant":
+            if residuals == "position":
+                residual_covariance = position_covariance(u[i], 0, nu)
+                blaah = 0
+            elif residuals == "beam":
+                residual_covariance = beam_covariance(u[i], v=0, nu=nu, broken_tile_fraction=broken_baselines_weight,
+                                                      calibration_type=calibration_type)
+                blaah = 0
+            elif residuals == 'both':
+                residual_covariance = position_covariance(u[i], 0, nu) + \
+                                      beam_covariance(u[i], v=0, nu=nu, broken_tile_fraction=broken_baselines_weight,
+                                                      calibration_type=calibration_type)
+        elif calibration_type == "sky":
+            if residuals == "sky":
+                residual_covariance = position_covariance(u[i], 0, nu)
+                blaah = 0
+            elif residuals == "beam":
+                residual_covariance = beam_covariance(u[i], v=0, nu=nu, broken_tile_fraction=broken_baselines_weight,
+                                                      calibration_type=calibration_type)
+                blaah = 0
+            elif residuals == 'both':
+                residual_covariance = position_covariance(u[i], 0, nu) + \
+                                      beam_covariance(u[i], v=0, nu=nu, broken_tile_fraction=broken_baselines_weight,
+                                                      calibration_type=calibration_type)
+
+        model_covariance = sky_covariance(u[i], 0, nu, S_low=1, S_high=10)
+        sky_residuals = sky_covariance(u[i], 0, nu, S_low=1e-3, S_high=1)
+        scale = numpy.diag(numpy.zeros_like(nu)) + 1
+        if weights is None:
+            nu_cov = 2*gain_averaged_covariance*model_covariance + \
+                     (scale + 2*gain_averaged_covariance)*sky_residuals
+        else:
+            nu_cov = 2*gain_averaged_covariance[i, ...]*model_covariance + \
+                     (scale + 2*gain_averaged_covariance[i, ...])*sky_residuals
+
+        cal_variance[i, :] = compute_power(nu, nu_cov)
+        raw_variance[i, :] = compute_power(nu, residual_covariance)
+
+    return raw_variance, cal_variance
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ssh", action="store_true", dest="ssh_key", default=False)
+    params = parser.parse_args()
+
+    import matplotlib
+
+    if params.ssh_key:
+        matplotlib.use("Agg")
+    from matplotlib import pyplot
+
+    main()
--- src/powerspectrum.py
+++ src/powerspectrum.py
@@ -1,5 +1,6 @@
 import numpy
 from scipy import signal
+from scipy.interpolate import interp1d
 
 
 def discrete_fourier_transform_matrix(nu):
@@ -30,3 +31,41 @@ def compute_power(nu, covariance):
 
     return power
 
+
+def read_data(ps_data_path):
+
+    z_fiducial = 8
+    bandwidth_fiducial = 30e6
+    central_frequency_fiducial = 1.42e9 / (z_fiducial + 1)
+
+    power_spectrum_fiducial_eor = numpy.loadtxt(ps_data_path, delimiter=',')
+    u_range_fiducial = numpy.linspace(0, 500, 100)
+    frequency_range_fiducial = numpy.linspace(central_frequency_fiducial - bandwidth_fiducial / 2,
+                                              central_frequency_fiducial + bandwidth_fiducial / 2, 251)
+
+    eta_fiducial = from_frequency_to_eta(frequency_range_fiducial)
+    # hist, eta_fiducial = numpy.histogram(eta_fiducial[:int(len(eta_fiducial) / 2)], bins=251//2)
+
+    return u_range_fiducial, eta_fiducial, power_spectrum_fiducial_eor.T
+
+
+def interpolate(u, eta, u_original, eta_original, ps_data):
+
+    eta_interpolated = numpy.zeros((len(u_original), len(eta)))
+    for i in range(len(u_original)):
+        eta_1d_interp = interp1d(eta_original, ps_data[i, :], kind='cubic')
+        eta_interpolated[i, :] = eta_1d_interp(eta)
+
+    fully_interpolated = numpy.zeros((len(u), len(eta)))
+    for i in range(len(eta)):
+        u_1d_interp = interp1d(u_original, eta_interpolated[:, i], kind='cubic')
+        fully_interpolated[:, i] = u_1d_interp(u)
+
+    return fully_interpolated
+
+
+def fiducial_eor_power_spectrum(u, eta, path = "./Data/"
+                 , file = "redshift8.csv"):
+    u_fiducial, eta_fiducial, ps_fiducial = read_data(path + file)
+    ps_interpolated = interpolate(u, eta, u_fiducial, eta_fiducial, ps_fiducial)
+    return ps_interpolated
