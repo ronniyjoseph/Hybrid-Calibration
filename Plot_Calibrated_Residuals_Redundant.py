357b18119f279d60bc84ac3a7e3e69b334ef8f66
Ronniy Joseph <ronniy.joseph@icrar.org>
2020-04-03 16:25:53 +0800

    Adapted to new modules

--- Plot_Calibrated_Residuals_Redundant.py
+++ Plot_Calibrated_Residuals_Redundant.py
@@ -4,42 +4,63 @@ from matplotlib import colors
 
 from src.powerspectrum import from_frequency_to_eta
 from src.powerspectrum import fiducial_eor_power_spectrum
+from src.radiotelescope import RadioTelescope
 
-from src.plottools import plot_power_spectrum
+from src.plottools import plot_2dpower_spectrum
 from src.plottools import plot_power_contours
 from src.generaltools import from_jansky_to_milikelvin
 
-from src.covariance import calculate_residual_error
-
+from src.covariance import calibrated_residual_error
+from src.covariance import compute_weights
 
 def main(labelfontsize = 16, ticksize= 11):
-    output_path = "/data/rjoseph/Hybrid_Calibration/theoretical_calculations/power_spectra/"
+    output_path = "/home/ronniyjoseph/Sync/PhD/Thesis/ThesisTex/images/chapter_7/"
     k_perp_range = numpy.array([1e-4, 1.1e-1])
     u_range = numpy.logspace(-1, numpy.log10(500), 100)
+
+    tile_diameter = 4
+    fraction_broken = 0.3
+    position_error = 0.01
+
+    telescope_position_path = "./Data/MWA_Compact_Coordinates.txt"
+    #telescope_position_path = "./Data/HERA_128.txt"
+
     frequency_range = numpy.linspace(135, 165, 251) * 1e6
     contour_levels = numpy.array([1e1, 1e2, 1e3])
 
     eta = from_frequency_to_eta(frequency_range)
-
     eor_power_spectrum = fiducial_eor_power_spectrum(u_range, eta)
 
-    position_raw, position_calibrated = calculate_residual_error(u=u_range, nu=frequency_range, residuals='position', calibration_type='redundant')
-    beam_raw, beam_calibrated = calculate_residual_error(u=u_range, nu=frequency_range, residuals='beam',calibration_type='redundant')
-    total_raw, total_calibrated = calculate_residual_error(u=u_range, nu=frequency_range, residuals='both',calibration_type='redundant')
+    telescope = RadioTelescope(load=True, path=telescope_position_path)
+    weights = compute_weights(u_range, telescope.baseline_table.u_coordinates,
+                              telescope.baseline_table.v_coordinates)
+
+    position_calibrated = calibrated_residual_error(u=u_range, nu=frequency_range, residuals='position',
+                                                                 calibration_type='redundant', weights = weights,
+                                                                 tile_diameter=tile_diameter,
+                                                    broken_baselines_weight = fraction_broken, position_error=position_error)
+    beam_calibrated = calibrated_residual_error(u=u_range, nu=frequency_range, residuals='beam',
+                                                         calibration_type='redundant', weights = weights,
+                                                                 tile_diameter=tile_diameter,
+                                                broken_baselines_weight = fraction_broken, position_error=position_error)
+    total_calibrated = calibrated_residual_error(u=u_range, nu=frequency_range, residuals='both',
+                                                           calibration_type='redundant', weights = weights,
+                                                                 tile_diameter=tile_diameter,
+                                                 broken_baselines_weight=fraction_broken, position_error=position_error)
 
     figure, axes = pyplot.subplots(1, 3, figsize=(15, 5))
 
     ps_norm = colors.LogNorm(vmin=1e3, vmax=1e15)
 
-    plot_power_spectrum(u_range, eta, frequency_range, position_calibrated, title="Position Error", axes=axes[0],
+    plot_2dpower_spectrum(u_range, eta, frequency_range, position_calibrated, title="Position Error", axes=axes[0],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=False,
                         xlabel_show=True, norm=ps_norm, ylabel_show=True)
 
-    plot_power_spectrum(u_range, eta, frequency_range, beam_calibrated, title="Beam Variations", axes=axes[1],
+    plot_2dpower_spectrum(u_range, eta, frequency_range, beam_calibrated, title="Beam Variations", axes=axes[1],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=False,
                         xlabel_show=True, norm=ps_norm, ylabel_show=False)
 
-    plot_power_spectrum(u_range, eta, frequency_range, total_calibrated, title="Total Error", axes=axes[2],
+    plot_2dpower_spectrum(u_range, eta, frequency_range, total_calibrated, title="Total Error", axes=axes[2],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
                         xlabel_show=True, norm=ps_norm, zlabel_show=True, ylabel_show=False)
 
@@ -52,7 +73,8 @@ def main(labelfontsize = 16, ticksize= 11):
     plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(total_calibrated, frequency_range)/eor_power_spectrum,
                         axes=axes[2], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True, norm=ps_norm, ylabel_show=False, contour_levels=contour_levels)
 
-    pyplot.savefig(output_path + "Calibrated_Residuals_Redundant.pdf")
+    pyplot.tight_layout()
+    pyplot.savefig(output_path + "Calibrated_Residuals_Redundant_MWA.pdf")
     pyplot.show()
     return
 
