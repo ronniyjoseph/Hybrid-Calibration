a625a5c252fbba2d416da4e22120c1ed61326811
Ronniy Joseph <ronniy.joseph@icrar.org>
2020-04-05 18:52:24 +0800

    Fixed parentheses bug in source count moments

--- src/skymodel.py
+++ src/skymodel.py
@@ -289,7 +289,10 @@ def sky_moment_returner(n_order, k1=4100, gamma1=1.59, k2=4100, gamma2=2.5, s_lo
         s_mid = s_high
     if s_low > s_mid:
         s_mid = s_low
-    moment = k1 / (n_order + 1 - gamma1) * (s_mid ** (n_order + 1 - gamma1)) - s_low ** (n_order + 1 - gamma1) + \
-             k2 / (n_order + 1 - gamma2) * (s_high ** (n_order + 1 - gamma2)) - s_mid ** (n_order + 1 - gamma2)
 
+    moment = k1 / (n_order + 1 - gamma1) * (s_mid ** (n_order + 1 - gamma1) - s_low ** (n_order + 1 - gamma1)) + \
+             k2 / (n_order + 1 - gamma2) * (s_high ** (n_order + 1 - gamma2) - s_mid ** (n_order + 1 - gamma2))
+
+    print(k1 / (n_order + 1 - gamma1) * (s_mid ** (n_order + 1 - gamma1) - s_low ** (n_order + 1 - gamma1)))
+    print(k2 / (n_order + 1 - gamma2) * (s_high ** (n_order + 1 - gamma2) - s_mid ** (n_order + 1 - gamma2)))
     return moment
